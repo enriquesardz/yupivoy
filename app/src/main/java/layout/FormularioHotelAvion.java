bd20dd06d22e915d8f5a894170a45faec6b941c2
Enrique Salas Rodriguez <enriquesaalas@gmail.com>
2017-04-05 16:16:58 -0500

    Modify the fragment layouts , adding functionality to the views now

--- app/src/main/java/layout/FormularioHotelAvion.java
+++ app/src/main/java/layout/FormularioHotelAvion.java
@@ -1,5 +1,6 @@
 package layout;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,8 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.ensardz.yupivoyenrique.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,9 +38,14 @@ public class FormularioHotelAvion extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+
+    EditText fechaEntradaEditText;
+    Calendar myCalendar;
+
     public FormularioHotelAvion() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
@@ -59,13 +72,48 @@ public class FormularioHotelAvion extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formulario_hotel_avion, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_formulario_hotel_avion, container, false);
+        fechaEntradaEditText = (EditText) view.findViewById(R.id.fecha_entrada);
+
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener fechaEntrada = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                actualizarFecha(); //Metodo actualizar fecha, cambia el contenido del textview
+            }
+        };
+
+        fechaEntradaEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), fechaEntrada,
+                        myCalendar .get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+        return view;
+
+    }
+
+    private void actualizarFecha(){
+        String formatoFecha = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(formatoFecha, Locale.US);
+        fechaEntradaEditText.setText(sdf.format(myCalendar.getTime()).toString());
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -75,16 +123,16 @@ public class FormularioHotelAvion extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
@@ -106,4 +154,8 @@ public class FormularioHotelAvion extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+//    class SetDate implements View.OnFocusChangeListener, DatePickerDialog.OnDateSetListener{
+//        private
+//    }
 }
